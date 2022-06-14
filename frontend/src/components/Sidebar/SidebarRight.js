ee2a9a99f653feead9d52ab93bedac03d5b181ee
Sadeeed <sadeeedw@gmail.com>
2022-06-14 20:12:00 +0500

    ⚰️ remove broken links

--- frontend/src/components/Sidebar/SidebarRight.js
+++ frontend/src/components/Sidebar/SidebarRight.js
@@ -20,7 +20,7 @@ const SidebarRight = (props) => {
         <h2 className="widget-title section-title">Archives</h2>
         <div className="widget-archive--list">
           <div className="archives-year">
-            <Link to="/archives/#2019">
+            <Link to="/"> 
               <span className="year">2019</span>
               <span className="count">5</span>
             </Link>
@@ -33,16 +33,16 @@ const SidebarRight = (props) => {
         </div>
         <h2 className="widget-title section-title">Categories</h2>
         <div className="tagCloud-tags">
-          <Link to="/categories/test/" className="font_size_2">
+          <Link to="/" className="font_size_2">
             Test
           </Link>
-          <Link to="/categories/syntax/" className="font_size_1">
+          <Link to="/" className="font_size_1">
             syntax
           </Link>
-          <Link to="/categories/test-with-whitespaces/" className="font_size_1">
+          <Link to="/" className="font_size_1">
             Test with whitespaces
           </Link>
-          <Link to="/categories/themes/" className="font_size_1">
+          <Link to="/" className="font_size_1">
             themes
           </Link>
         </div>
@@ -53,32 +53,14 @@ const SidebarRight = (props) => {
         </div>
         <h2 className="widget-title section-title">Tags</h2>
         <div className="tagCloud-tags">
-          <Link to="/tags/markdown/" className="font_size_2">
+          <Link to="/" className="font_size_2">
             markdown
           </Link>
-          <Link to="/tags/css/" className="font_size_1">
+          <Link to="/" className="font_size_1">
             css
           </Link>
-          <Link to="/tags/emoji/" className="font_size_1">
+          <Link to="/" className="font_size_1">
             emoji
-          </Link>
-          <Link to="/tags/html/" className="font_size_1">
-            html
-          </Link>
-          <Link to="/tags/privacy/" className="font_size_1">
-            privacy
-          </Link>
-          <Link to="/tags/shortcodes/" className="font_size_1">
-            shortcodes
-          </Link>
-          <Link to="/tags/tag-with-whitespaces/" className="font_size_1">
-            tag with whitespaces
-          </Link>
-          <Link to="/tags/text/" className="font_size_1">
-            text
-          </Link>
-          <Link to="/tags/themes/" className="font_size_1">
-            themes
           </Link>
         </div>
       </section>
