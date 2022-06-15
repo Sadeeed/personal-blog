38f2229e5c2e91fff0711e12b6686ef9175dd03d
Sadeeed <sadeeedw@gmail.com>
2022-06-15 18:24:20 +0500

    🚀 ready for deployment

--- backend/index.js
+++ backend/index.js
@@ -13,9 +13,10 @@ import { cfg } from "./config.js";
 const app = express();
 const port = 5000;
 
-// const uri = 'mongodb+srv://icesoup:pegasis8@aloo.e7een.mongodb.net/?retryWrites=true&w=majority'
 const uri =
-  "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
+  "mongodb+srv://icesoup:pegasis8@aloo.e7een.mongodb.net/?retryWrites=true&w=majority";
+// const uri =
+//   "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000";
 
 mongoose
   .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
